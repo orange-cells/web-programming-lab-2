c84aa59607455c03ebaa82d87e653491d0ad3ddd
orange-cells <sokolovaaveraa@gmail.com>
2026-02-21 14:59:38 +0300

    реализовала добавление задачи в localStorage

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 const loadPage = () => {
-    const header = document.createElement('h1')
-    header.textContent = 'To-Do List'
-    document.querySelector('body').append(header)
+    // заголовок
+    const header = document.createElement('h1');
+    header.textContent = 'To-Do List';
+    document.querySelector('body').append(header);
 
-    // будет выплывать окно для создания
-    const addButton = document.createElement('button')
-    document.querySelector('body').append(addButton)
+    // добавление задачи
+    const addButton = document.createElement('button');
+    addButton.textContent = '+';
 
-    const search = document.createElement('input')
-    search.setAttribute('placeholder', 'Searching for...')
-    document.querySelector('body').append(search)
+    // поиск задач
+    const search = document.createElement('input');
+    search.setAttribute('placeholder', 'Searching for...');
+    document.querySelector('body').append(search);
 
-    const tasks = document.createElement('form')
-    document.querySelector('body').append(tasks)
+    // создание задачи
+    const taskTable = document.createElement('form');
+    document.querySelector('body').append(taskTable);
+    
+    const task = document.createElement('input');
+    task.setAttribute('id', 'taskText');
+    task.setAttribute('type', 'text');
+    task.setAttribute('placeholder', 'Enter a task');
+
+    const taskDate = document.createElement('input');
+    taskDate.setAttribute('id', 'taskDate');
+    taskDate.setAttribute('type', 'date');
+
+    taskTable.append(task);
+    taskTable.append(taskDate);
+    taskTable.append(addButton);
+
+    addButton.addEventListener('click', newTask);
 }
+
+const taskTable = JSON.parse(localStorage.getItem('taskTable')) || [];
+
+const newTask = () => {
+    taskTable.push({
+        taskText: document.querySelector('#taskText').value.trim(),
+        taskDate: document.querySelector('#taskDate').value,
+        status: false
+    });
+
+    localStorage.setItem('taskTable', JSON.stringify(taskTable));
+}
+
 
 document.addEventListener('DOMContentLoaded', loadPage)
