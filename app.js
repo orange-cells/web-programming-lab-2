154be6976d7c72fbc7f2bde51430507243f6e37e
orange-cells <sokolovaaveraa@gmail.com>
2026-02-24 00:38:47 +0300

    реализовала отображение списка задач

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ const loadPage = () => {
     taskTable.append(taskDate);
     taskTable.append(addButton);
 
+    const ul = document.createElement('ul');
+    ul.setAttribute('id', 'taskList');
+    document.querySelector('body').append(ul);
+
     addButton.addEventListener('click', newTask);
 }
 
@@ -37,13 +41,37 @@ const taskTable = JSON.parse(localStorage.getItem('taskTable')) || [];
 
 const newTask = () => {
     taskTable.push({
-        taskText: document.querySelector('#taskText').value.trim(),
-        taskDate: document.querySelector('#taskDate').value,
+        taskText: document.getElementById('taskText').value,
+        taskDate: document.getElementById('taskDate').value,
         status: false
     });
 
     localStorage.setItem('taskTable', JSON.stringify(taskTable));
 }
 
+const displayTaskTable = () => {
+    const tasks = document.getElementById('taskList');
 
-document.addEventListener('DOMContentLoaded', loadPage)
+    taskTable.forEach(element => {
+        const task = document.createElement('li');
+        
+        const checkBox = document.createElement('input');
+        // checkBox.setAttribute('id', )
+        checkBox.setAttribute('type', 'checkbox');
+        
+        const text = document.createElement('span');
+        text.textContent = element.taskText;
+
+        const date = document.createElement('span');
+        date.textContent = element.taskDate;
+
+        task.append(checkBox, text, date);
+        tasks.appendChild(task);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadPage();
+    displayTaskTable();
+});
